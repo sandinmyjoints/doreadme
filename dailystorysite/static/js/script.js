5d15acc4dce2000882d8c5d077f741eb361cf49a
William Bert <wbert@ubuntu.(none)>
2012-02-04 10:21:15 -0500

    A lot of styling work.

--- dailystorysite/static/js/script.js
+++ dailystorysite/static/js/script.js
@@ -1,7 +1,15 @@
 /* Author:
 
-*/
+ */
 
+$(document).ready(function () {
+    $("p.teaser").hover(function (event) {
+            $(this).find("a.full_story").css("opacity", 1);
+        },
+        function (event) {
+            $(this).find("a.full_story").css("opacity", .2);
+        });
+});
 
 
 
