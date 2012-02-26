b3f9477a75758fb953f45f1459c19a80efe893b5
William Bert <william.bert@gmail.cm>
2012-02-26 12:29:34 -0500

    Journal model now has a featured_stories method that looks up all self's featured stories. Using that in the journal templates instead of the prior custom view. Is this violating MVC? Not sure.

--- dailystorysite/context_processors.py
+++ dailystorysite/context_processors.py
@@ -1,7 +1,7 @@
-from django.conf import settings
 from django.contrib.sites.models import Site
 
 __author__ = 'wbert'
 
 def site_name(request):
     return {"SITE_NAME": Site.objects.get_current().name }
+
--- journal/urls.py
+++ journal/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls.defaults import url, patterns
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from journal.models import Journal
-from journal.views import JournalDetailView
 
 urlpatterns = patterns('',
 
+    url(r'^list/$', ListView.as_view(model=Journal,
+        context_object_name="journal_list",
+        template_name="journal/journal_list.html"), {}, name="journal_list"),
+
     # /journal/journal-slug -> Journal detail view (which contains a list of all the Journal's Stories that have been featured on a Day)
-    url(r'^(?P<slug>[\w-]+)/$', JournalDetailView.as_view(model=Journal,
+    url(r'^(?P<slug>[\w-]+)/$', DetailView.as_view(model=Journal,
         queryset=Journal.objects.all(),
         context_object_name="journal",
         template_name='journal/journal.html'), {}, name="journal_detail"),
+
+
 )
