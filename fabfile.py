6d559e36f5d2f2fbba9ef9186cf0c939a8958885
William Bert <william.bert@gmail.cm>
2012-02-27 22:05:24 -0500

    Wrote initial fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,68 @@
 __author__ = 'wbert'
 
-# What do I need to do to deploy?
+# What do I need to do to initial deploy?
 # pip install all python libs
 # set up postgres database
 # git archive, then unpack and install source
 # restart apache
+
+# Update deploy
+# on dev:
+# git push origin master  (puts it on webfaction /git repo)
+#
+# on prod:
+# pull from origin
+# possibly run python manage.py collectstatic
+# possibly run syncdb
+# possibly do south migration
+# apache2/bin/restart
+
+# import fabrics API functions - self-explanatory once you see
+from fabric.api import *
+
+PROD_HOST = 'wjb@wjb.webfactional.com:22'
+PROD_DIR = '/home/wjb/webapps/doreadme/doreadme'
+
+def prod():
+    """Set up the environment for the current production server."""
+    env['hosts'] = [PROD_HOST]
+#    env.roledefs["prod"] = [PROD_HOST]
+    env['dir'] = PROD_DIR
+
+def push():
+    """On local, push latest commit to master to origin."""
+    local('git push origin master') # runs the command on the local environment
+
+def pull():
+    """On prod, pull the latest from origin/master."""
+    with cd(env['dir']):
+        run('git pull origin master') # runs the command on the remote environment
+
+def restart_apache():
+    """Restart apache"""
+    with cd(env['dir']):
+        run('../apache2/bin/restart')
+
+def collectstatic():
+    """Run manage.py collectstatic, suppressing any input."""
+    with cd(env['dir']):
+        run('python manage.py collectstatic --noinput')
+
+def migrate(*args):
+    """South migration"""
+    with cd(env['dir']):
+        run('python manage.py migrate ' + " ".join(args))
+
+def revert():
+    """ Revert git via reset --hard @{1} """
+    with cd(env['dir']):
+        run('git reset --hard @{1}')
+        restart_apache()
+
+def deploy():
+    """Deploy to prod. Calls prod(), push(), pull(), restart_apache()"""
+    # TODO figure out how to specify whether to collecstatic and migrate.
+    print "Env hosts: %s" % env.hosts
+    push()
+    pull()
+    restart_apache()
