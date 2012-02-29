cde646cfd1691b59943b2b3bb2cd492b1b17ae90
William Bert <william.bert@gmail.cm>
2012-02-28 23:00:53 -0500

    Quick and dirty fix to DayAdmin so I can see which Journal a Story is in and whether it's verified or not.

--- day/admin.py
+++ day/admin.py
@@ -1,4 +1,13 @@
 from day.models import Day
 from django.contrib import admin
 
-admin.site.register(Day)
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('day_and_story_and_journal',)
+
+    def day_and_story_and_journal(self, obj):
+        if obj.story:
+            return ("%s (%s) %s" % (obj, obj.story.journal, "(Verified)" if obj.story.verified else ""))
+    day_and_story_and_journal.short_description = 'Day, Story, Journal'
+
+admin.site.register(Day, DayAdmin)
