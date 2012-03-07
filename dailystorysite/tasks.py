169cc85ffb9e198da237276647483e86d44ef22d
William Bert <william.bert@teamcarney.com>
2012-03-07 18:58:45 -0500

    Added a lot of logging to select_story_for_next_day

--- dailystorysite/tasks.py
+++ dailystorysite/tasks.py
@@ -31,11 +31,19 @@ def select_story_for_next_day():
     logger.info("select_story_for_next_day")
 
     try:
+        logger.info("in try")
+
         today = date.today()
+        logger.info("today is %s" % today)
+
         tomorrow = date.fromordinal(today.toordinal()+1)
+        logger.info("tomorrow is %s" % tomorrow)
+
         day_after_tomorrow = date.fromordinal(tomorrow.toordinal()+1)
+        logger.info("day_after_tomorrow is %s" % day_after_tomorrow)
 
         today_days = Day.objects.filter(day__gte=today).filter(day__lte=tomorrow)
+        logger.info("today_days.count() is %d" % today_days)
 
         if today_days.count() < 1:
             logger.warn("No Day for today. Creating one.")
@@ -43,11 +51,14 @@ def select_story_for_next_day():
         else:
             today_day = today_days[0]
             if today_days.count() > 1:
-                logger.warn("Multiple Days for today! This violates the db constraint on Day so should never happen. today_days.count() is %d." % tomorrow_days.count())
+                logger.warn("Multiple Days for today! This violates the db constraint on Day so should never happen. today_days.count() is %d." % today_days.count())
+
+        logger.info("now today_day is %s" % today_day)
 
         if not today_day.story:
             logger.warn("No story set for today! Setting one.")
             today_day.story = Day.find_random_unfeatured_story(num_days_recent=settings.NUM_DAYS_RECENT)
+            logger.info("today_day.story is %s" % today_day.story)
 
         tomorrow_days = Day.objects.filter(day__gte=tomorrow).filter(day__lte=day_after_tomorrow)
         logger.debug("Selecting story for %s. tomorrow_days.count() is %d." % (tomorrow, tomorrow_days.count()))
@@ -61,9 +72,12 @@ def select_story_for_next_day():
         else:
             tomorrow_day = Day.objects.create(day=tomorrow)
 
+        logger.info("now tomorrow_day is %s" % tomorrow_day)
+
         if not tomorrow_day.story:
             # As long as Day.save() is also calling find_random_unfeatured_story, this should not be called
             tomorrow_day.story = Day.find_random_unfeatured_story(num_days_recent=settings.NUM_DAYS_RECENT)
+            logger.info("tomorrow_day.story is %s" % tomorrow_day.story)
 
     except Exception, exc:
         logger.warn("Exception in select_story_for_tomorrow. Retrying. Exception was '%s'." % exc)
